d55e575ad37ab4cd5e9028daa3328853b07f176d
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2024-02-10 21:32:42 +0200

    refactor: :recycle: Update server

--- Server/server.js
+++ Server/server.js
@@ -15,10 +15,6 @@ app.get("/style.css", (req, res) => {
   res.sendFile(join(__dirname, "../Client/style.css"));
 });
 
-app.get("/script.js", (req, res) => {
-  res.sendFile(join(__dirname, "../Client/script.js"));
-});
-
 app.get("/chat.svg", (req, res) => {
   res.sendFile(join(__dirname, "../Client/chat.svg"));
 });
